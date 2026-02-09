be06d8aa07ec903d1ccf3835912eafdf1d749187
Shivansh <shivanshpatra.patra@gmail.com>
2026-02-09 22:47:59 +0530

    Create create_table.sql

---
+++ create_table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE SupplyChainLPI (
+    Country_Name VARCHAR(100),
+    Country_Code VARCHAR(10),
+    Year INT,
+    LPI_Score DECIMAL(5,2),
+    Customs DECIMAL(5,2),
+    Infrastructure DECIMAL(5,2),
+    International_Shipments DECIMAL(5,2),
+    Logistics_Quality DECIMAL(5,2),
+    Tracking_Tracing DECIMAL(5,2),
+    Timeliness DECIMAL(5,2)
+);
